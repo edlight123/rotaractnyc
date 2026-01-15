eabd8a8b3ad2c83c637dd2cba38c93566110418c
edlight123 <info@edlight.org>
2026-01-15 03:12:29 +0000

    Configure Next.js to ignore ESLint errors during builds

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ['images.squarespace-cdn.com', 'static1.squarespace.com', 'firebasestorage.googleapis.com'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
